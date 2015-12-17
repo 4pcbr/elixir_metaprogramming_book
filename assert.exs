a91bc9e064be07651cc3f39530e4d9415c97f55d
4pcbr <me@4pcbr.com>
2015-12-17 23:36:12 +0100

    Implemented bool assert and refute macro in assert exercise

--- assert.exs
+++ assert.exs
@@ -5,6 +5,27 @@ defmodule Assertion do
       Assertion.Test.assert( operator, lhs, rhs )
     end
   end
+  defmacro assert( expr ) do
+    quote bind_quoted: [ expr: expr ] do
+      Assertion.Test.assert( expr )
+    end
+  end
+  defmacro refute({ operator, meta, [ lhs, rhs ] }) do
+    inversed_operator = case operator do
+      :==   -> :!=
+      :>    -> :<
+      :<    -> :>
+      :!=   -> :==
+    end
+    quote bind_quoted: [ inversed_operator: inversed_operator, lhs: lhs, rhs: rhs, meta: meta ] do
+      Assertion.Test.assert( inversed_operator, lhs, rhs )
+    end
+  end
+  defmacro refute( expr ) do
+    quote bind_quoted: [ expr: !expr ] do
+      Assertion.Test.assert( expr )
+    end
+  end
 
   defmacro __using__( _options ) do
     quote do
@@ -54,6 +75,7 @@ defmodule Assertion.Test do
     :ok
   end
   def assert( :==, lhs, rhs ) do
+    IO.inspect { lhs, rhs }
     { :fail, """
       Expected:       #{lhs}
       to be equal to: #{rhs}
@@ -70,6 +92,39 @@ defmodule Assertion.Test do
       """
     }
   end
+  def assert( :<, lhs, rhs ) when lhs < rhs do
+    :ok
+  end
+  def assert( :<, lhs, rhs ) do
+    { :fail, """
+      Expected:           #{lhs}
+      to be less than:    #{rhs}
+      """
+    }
+  end
+  def assert(:"!=", lhs, rhs) when lhs != rhs do
+    :ok
+  end
+  def assert(:"!=", lhs, rhs) do
+  {
+    :fail,
+    """
+    Expected:             #{lhs}
+    to not be equal to    #{rhs}
+    """
+  }
+  end
+  def assert( expr ) when is_boolean( expr ) and expr == true do
+    :ok
+  end
+  def assert( expr ) when is_boolean( expr ) do
+    {
+      :fail,
+      """
+      Expected true, got false
+      """
+    }
+  end
 
 end
 
@@ -79,12 +134,22 @@ defmodule MathTest do
   test "integers can be added and subtracted" do
     assert 1 + 1 == 2
     assert 2 + 3 == 5
-    assert 5 - 5 == 10
+    assert 5 - 5 != 10
+    assert 2 > 1
+    assert 2 < 3
+    refute 3 < 2
+    refute 5 != 5
+    refute 1 + 1 != 2
   end
 
   test "integers can be multiplied and divided" do
     assert 5 * 5 == 25
     assert 10 / 2 == 5
+  end
+
+  test "test bool" do
+    assert true
+    refute false
   end
 
 end
