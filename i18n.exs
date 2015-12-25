c4f0c89ba5a92337f0f76e557d8b8309313b681b
4pcbr <me@4pcbr.com>
2015-12-25 19:02:53 +0100

    Implemented Plural.(EN|FR|RU) modules

--- i18n.exs
+++ i18n.exs
@@ -1,3 +1,39 @@
+defmodule Plural do
+
+  def pluralize( item, locale ), do: pluralize( item, locale, 1 )
+  def pluralize( item, _locale, _count ) when is_binary( item ), do: item
+  def pluralize( item, locale, count ) when is_list( item ) do
+    module = Module.concat( __MODULE__, String.upcase( locale ) )
+    key = case :code.is_loaded( module ) do
+      false -> raise "The locale #{locale} is not supported"
+      true -> module.get_plural_form(count)
+    end
+  end
+
+end
+
+defmodule Plural.EN do
+  def get_plural_form( count ) when count == 1, do: :one
+  def get_plural_form( _count )               , do: :other
+end
+
+defmodule Plural.FR do
+  def get_plural_form( count ) when is_integer( count ) and count in [ 0, 1 ], do: :one
+  def get_plural_form( count ) when is_float( count ) and count < 2          , do: :one
+  def get_plural_form( _count )                                              , do: :other
+end
+
+defmodule Plural.RU do
+  def get_plural_form( count ) when is_integer( count ) and rem( count, 10 ) == 1 and rem( count, 100 ) != 11, do: :one
+  def get_plural_form( count ) when is_integer( count ) and rem( count, 10 ) == 2 and not( rem( count, 100 ) in 12..14 ), do: :few
+  def get_plural_form( count ) when is_integer( count ) and ( ( rem( count, 10 ) in [ 0, 5, 6, 7, 8, 9 ] ) or ( rem( count, 100 ) in 11..14 ) ), do: :many
+  def get_plural_form( _count ), do: :other
+end
+
+
+
+
+
 defmodule Translator do
   defmacro __using__( _options ) do
     quote do
@@ -65,6 +101,10 @@ defmodule I18n do
 
   use Translator
 
+  # http://unicode.org/repos/cldr-tmp/trunk/diff/supplemental/language_plural_rules.html
+  # For pluralization rules we're going to use the
+  # conventional keys: one / two / few / many / other
+
   locale "en",
     flash: [
       hello: "Hello %{first} %{last}!",
@@ -74,7 +114,10 @@ defmodule I18n do
       title: "Users",
     ],
     title: [
-      user: "user",
+      user: [
+        one: "user",
+        other: "users",
+      ],
     ]
 
   locale "fr",
@@ -86,7 +129,26 @@ defmodule I18n do
       title: "Utilisateurs",
     ],
     title: [
-      user: "utilisateurs",
+      user: [
+        one:   "utilisateur",
+        other: "utilisateurs",
+      ],
+    ]
+  locale "ru",
+    flash: [
+      hello: "Привет %{first} %{last}!",
+      bye:   "Всего хорошего, %{name}!",
+    ],
+    users: [
+      title: "Пользователи",
+    ],
+    title: [
+      user: [
+        one:   "пользователь",
+        few:   "пользователя",
+        many:  "пользователей",
+        other: "пользователей"
+      ],
     ]
 
 end
