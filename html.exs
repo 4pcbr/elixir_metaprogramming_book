ac8f2d5fa72195e914e96ce36209aff9966691d5
4pcbr <me@4pcbr.com>
2015-12-31 12:46:12 +0100

    Implemented HTML DSL lib exercise

--- html.exs
+++ html.exs
@@ -1,7 +1,24 @@
 defmodule Html do
 
+  @external_resource tags_path = Path.join([ __DIR__, "tags.txt" ])
+  @tags ( for line <- File.stream!( tags_path, [], :line ) do
+    line |> String.strip |> String.to_atom
+  end)
+
+  for tag <- @tags do
+    defmacro unquote( tag )( attrs, do: inner ) do
+      tag = unquote( tag )
+      quote do: tag( unquote( tag ), unquote( attrs ), do: unquote( inner ) )
+    end
+    defmacro unquote( tag )( attrs \\ [] ) do
+      tag = unquote( tag )
+      quote do: tag( unquote( tag ), unquote( attrs ) )
+    end
+  end
+
   defmacro markup( do: block ) do
     quote do
+      import Kernel, except: [ div: 2 ]
       { :ok, var!( buffer, Html ) } = start_buffer([])
       unquote( block )
       result = render( var!( buffer, Html ) )
@@ -18,12 +35,22 @@ defmodule Html do
 
   def render( buff ), do: Agent.get( buff, &(&1) ) |> Enum.reverse |> Enum.join("")
 
-  defmacro tag( name, do: inner ) do
+  defmacro tag( name, attrs \\ [] ) do
+    { inner, attrs } = Dict.pop( attrs, :do )
+    quote do: tag( unquote( name ), unquote( attrs ), do: unquote( inner ) )
+  end
+  defmacro tag( name, attrs, do: inner ) do
     quote do
-      put_buffer var!( buffer, Html ), "<#{unquote(name)}>"
+      put_buffer var!( buffer, Html ), open_tag( unquote_splicing([ name, attrs ]))
       unquote( inner )
-      put_buffer var!( buffer, Html ), "</#{unquote(name)}>"
+      put_buffer var!( buffer, Html ), "</#{unquote( name )}>"
     end
+  end
+
+  def open_tag( name, [] ), do: "<#{name}>"
+  def open_tag( name, attrs ) do
+    attr_html = for { key, val } <- attrs, into: "", do: " #{key}=\"#{val}\""
+    "<#{name}#{attr_html}>"
   end
 
   defmacro text( string ) do
---
+++ template_render.exs
@@ -0,0 +1,31 @@
+defmodule Template do
+
+  import Html
+
+  def render do
+    markup do
+      div id: "main" do
+        h1 class: "title" do
+          text "Welcome"
+        end
+      end
+      div class: "row" do
+        div do
+          p do: text "Hello!"
+        end
+      end
+      tag :table do
+        tag :tr do
+          for i <- 0..5 do
+            tag :td, do: text( "Cell #{i}" )
+          end
+        end
+      end
+      div do
+        text "Some text"
+      end
+    end
+  end
+
+end
+
