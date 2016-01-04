e9749f2130c58787600818287e4f5bdf91d0ae2f
4pcbr <me@4pcbr.com>
2016-01-04 20:34:09 +0100

    More torments on the indentation

--- indenter.exs
+++ indenter.exs
@@ -3,11 +3,23 @@ defmodule Indenter do
   @tab_indent "  "
 
   defp do_indent( [ tag, next | tail ], acc, offset \\ 0 ) do
+    res = case tag_type( tag ) do
+      :opening_tag ->
+        acc <> ( List.duplicate( @tab_indent, offset ) |> Enum.join ) <> tag
+      :closing_tag ->
+        acc <> ( List.duplicate( @tab_indent, offset ) |> Enum.join ) <> tag
+      :no_tag ->
+
+    end
 
   end
 
   defp tag_type( tag ) do
-    #TODO
+    case Regex.run( ~r/^<\/?/, to_string( tag ) ) do
+      "<"  -> :opening_tag
+      "</" -> :closing_tag
+      _    -> :no_tag
+    end
   end
 
 end
