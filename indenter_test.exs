b81381f3fde98cd973a2dcf74b8de97cc37fc684
4pcbr <me@4pcbr.com>
2016-01-05 22:10:47 +0100

    Indentier tests

---
+++ indenter_test.exs
@@ -0,0 +1,42 @@
+Code.require_file( "indenter.exs", __DIR__ )
+
+defmodule IndenterTest do
+
+  use ExUnit.Case
+  import Indenter, only: [ indent: 1 ]
+
+  test "Indents an empty tag as a single line" do
+    tags = ~w(<tag> </tag>)
+    assert indent( tags ) == "<tag></tag>" 
+  end
+
+  test "Indents a tag with no-tag content as a single line" do
+    tags = ~w(<tag> content </tag>)
+    assert indent( tags ) == "<tag>content</tag>"
+  end
+
+  test "Indents a tag with a nested tag with newlines and a padding" do
+    tags = ~w(<parent> <child> </child> </parent>)
+    assert indent( tags ) == """
+    <parent>
+      <child>
+      </child>
+    </parent>
+    """
+  end
+
+  test "Increments the padding for nested tags" do
+    tags = ~w(<t1> <t2> <t3> <t4> asd </t4> </t3> </t2> </t1>)
+    assert indent( tags ) == """
+    <t1>
+      <t2>
+        <t3>
+          <t4>asd</t4>
+        </t3>
+      </t2>
+    </t1>
+    """
+  end
+
+end
+
